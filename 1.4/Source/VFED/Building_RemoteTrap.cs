b9261144a117282613498aa1e2d946f89967d67c
Joshua <legodude17@users.noreply.github.com>
2023-03-21 17:10:31 -0700

    Remote Charge code

---
+++ 1.4/Source/VFED/Building_RemoteTrap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace VFED;
+
+[StaticConstructorOnStartup]
+public abstract class Building_RemoteTrap : Building_Trap
+{
+    protected override float SpringChance(Pawn p) => 0f;
+
+    public override IEnumerable<Gizmo> GetGizmos() =>
+        base.GetGizmos()
+           .Append(new Command_Action
+            {
+                defaultLabel = "VFED.Detonate".Translate(),
+                icon = TexDeserters.DetonateTex,
+                action = delegate
+                {
+                    Spring(this.GetRegion().ListerThings.ThingsInGroup(ThingRequestGroup.Pawn).OfType<Pawn>().RandomElementWithFallback()
+                        ?? Map.mapPawns.AllPawnsSpawned.RandomElementWithFallback());
+                }
+            });
+}
+
+public class Building_RemoteTrapExplosive : Building_RemoteTrap
+{
+    protected override void SpringSub(Pawn p)
+    {
+        GetComp<CompExplosive>().StartWick();
+    }
+}
