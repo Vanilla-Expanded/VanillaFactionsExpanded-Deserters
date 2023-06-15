9bae0eb4c1b7f73e76b2b05eef36171945dfaaab
Joshua <legodude17@users.noreply.github.com>
2023-06-15 10:31:48 -0700

    Fix Biosecured crates

--- 1.4/Source/VFED/Things/Building_CrateBiosecured.cs
+++ 1.4/Source/VFED/Things/Building_CrateBiosecured.cs
@@ -1,4 +1,6 @@
-﻿using Verse;
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
 
 namespace VFED;
 
@@ -6,7 +8,14 @@ public class Building_CrateBiosecured : Building_SupplyCrate
 {
     protected override void GenerateContents()
     {
-        for (var i = 0; i < 3; i++) innerContainer.TryAdd(ThingMaker.MakeThing(ContrabandManager.AllContraband.RandomElement().Item1));
+        for (var i = 0; i < 3; i++)
+        {
+            var (def, ext) = ContrabandManager.AllContraband.RandomElement();
+            var thing = ThingMaker.MakeThing(def, def.MadeFromStuff ? GenStuff.DefaultStuffFor(def) : null);
+            thing.stackCount = ext.useCriticalIntel ? Mathf.CeilToInt(3f / ext.intelCost) : Mathf.CeilToInt(10f / ext.intelCost);
+            innerContainer.TryAdd(thing);
+        }
+
         if (Rand.Chance(0.25f))
         {
             var intel = ThingMaker.MakeThing(VFED_DefOf.VFED_CriticalIntel);
